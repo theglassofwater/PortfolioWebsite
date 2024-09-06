b9e5daa83b048f4f2aa8f5b7f72dcea351b5d380
theglassofwater <joeace2002@gmail.com>
2024-09-06 12:06:12 +0100

    Cors_allowed_origins commented

--- WebApp/backend/settings.py
+++ WebApp/backend/settings.py
@@ -45,8 +45,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000/",
-    "http://localhost:3000/",
+    # "http://127.0.0.1:8000/",
+    # "http://localhost:3000/",
 ]
 
 ROOT_URLCONF = 'backend.urls'
