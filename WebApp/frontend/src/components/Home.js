5906ae34073e9760dd490b299076fb27ba150789
theglassofwater <joeace2002@gmail.com>
2024-09-06 16:55:34 +0100

    adding stuff to home page

--- WebApp/frontend/src/components/Home.js
+++ WebApp/frontend/src/components/Home.js
@@ -1,8 +1,38 @@
 import React from "react";
+import YeImage from "../assets/logos/YE-192x192.png";
+import gitIcon from "../assets/github_icon.svg";
+import linkedinIcon from "../assets/linkedin_icon.svg";
+import CV from "../assets/CV_Base_Data.pdf";
 
 const Home = () => {
     return (
-        <div> Home dfjsnifjdsnf sdjfnsojdfnsnk fsjodfnjosfnsojd ffjonfdsojf sdfnosdf sjdfjosd fs dfjosd fjn </div>
+        <section id="home">  
+            <div> 
+                <img src={YeImage} alt="Youssef El Aasar" />
+            </div>
+            <div>
+                <h1>Youssef 
+                    <br />
+                    El Aasar
+                </h1>
+                <h2>Computer Science Masters Graduate</h2>
+                <span>
+                    <a href="https://www.linkedin.com/in/youssefelaasar" target="_blank">
+                        <img src={linkedinIcon} alt="LinkedIn Icon" />
+                    </a>
+
+                    <a href="https://www.github.com/theglassofwater" target="_blank">
+                        <img src={gitIcon} alt="GitHub Icon" />
+                    </a>
+                </span>
+                <p>
+                    Curios programmer with a passion for learning and problem solving.
+                </p>
+                <a href={CV} target="_blank">  
+                    <button download>Download CV</button>
+                </a>
+            </div>
+        </section>
     );
 }
 
