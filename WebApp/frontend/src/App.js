2dddbffdd8cb086edf4148b5ae7ea4e2a390634d
theglassofwater <joeace2002@gmail.com>
2024-09-06 12:02:53 +0100

    Trying to set up NavBar

--- WebApp/frontend/src/App.js
+++ WebApp/frontend/src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About'; 
-import Create from './components/Create';
+import Projects from './components/Projects'; 
+import Constact from './components/Contact';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<Create />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Constact />} />
       </Routes>
     </div>
   );
--- WebApp/frontend/src/index.js
+++ WebApp/frontend/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {StyledEngineProvider} from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <React.StrictMode>
-      <App />
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
     </React.StrictMode>
   </Router>
 );
