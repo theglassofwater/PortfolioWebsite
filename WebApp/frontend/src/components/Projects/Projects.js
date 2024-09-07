fcfbdcd8d2d35c443e95f893c2fdf3377a507dd7
theglassofwater <joeace2002@gmail.com>
2024-09-07 17:50:43 +0100

    project section

--- WebApp/frontend/src/components/Projects/Projects.js
+++ WebApp/frontend/src/components/Projects/Projects.js
@@ -7,36 +7,37 @@ const Projects = () => {
     <section id="projects" className={styles.container}>
         <h1 className="SectionTitle">Projects</h1>
         <div className={styles.ProjectsContainer}>
-            <div>
-                <h2>Music Generator</h2>
+            <div>   
                 <a href="https://www.github.com/theglassofwater/project1" target="_blank">
+                    <h2>Music Generator</h2>
                     <img src={Project1Image} alt="Project 1" />
                 </a>
                 <p>Description of Project 1 dsffnjdsnfdsnfjods fjdsnfjosdnf jisd fjosndfosd fjsof dsfnodsf</p>
             </div>
     
             <div>
-                <h2>Project 2</h2>
                 <a href="https://www.github.com/theglassofwater/project2" target="_blank">
-                    <img src="https://via.placeholder.com/150" alt="Project 2" />
+                    <h2>Project 2</h2>
+                    <img src={Project1Image} alt="Project 2" />
                 </a>
-                <p>Description of Project 2</p>
+                <p>Description of Project 1 dsffnjdsnfdsnfjods fjdsnfjosdnf jisd fjosndfosd fjsof dsfnodsf</p>
             </div>
     
-            <div>
-                <h2>Project 3</h2>
+            <div>                    
                 <a href="https://www.github.com/theglassofwater/project3" target="_blank">
-                    <img src="https://via.placeholder.com/150" alt="Project 3" />
+                    <h2>Project 3</h2>
+                    <img src={Project1Image} alt="Project 3" />
                 </a>
-                <p>Description of Project 3</p>
+                <p>Description of Project 1 dsffnjdsnfdsnfjods fjdsnfjosdnf jisd fjosndfosd fjsof dsfnodsf</p>
             </div>
     
             <div>
-                <h2>Project 4</h2>
                 <a href="https://www.github.com/theglassofwater/project4" target="_blank">
-                    <img src="https://via.placeholder.com/150" alt="Project 4" />
+                    <h2>Project 4</h2>
+                    <img src={Project1Image} alt="Project 4" />
                 </a>
-                <p>Description of Project 4</p>
+
+                <p>Description of Project 1 dsffnjdsnfdsnfjods fjdsnfjosdnf jisd fjosndfosd fjsof dsfnodsf</p>
             </div>
         </div>
     </section>
