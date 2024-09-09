40d2f263d335b37a3658cc1a6d71a7f8b318b21a
theglassofwater <joeace2002@gmail.com>
2024-09-09 16:28:45 +0100

    Finished base css and js

--- WebApp/frontend/src/components/Contact/Contact.js
+++ WebApp/frontend/src/components/Contact/Contact.js
@@ -5,9 +5,21 @@ const Contact = () => {
     return (
         <section id="contact" className={styles.container}>
             <h1 className="sectionTitle">contact</h1>
-            <div>
-                
-            </div>
+            <form>
+                <div className="">
+                    <label htmlFor="name" hidden>Name</label>
+                    <input type="text" id="name" name="name" placeholder="Name" required />
+                </div>
+                <div className="">
+                    <label htmlFor="Email" hidden>Email</label>
+                    <input type="text" id="Email" name="Email" placeholder="Email" required />
+                </div>
+                <div className="">
+                    <label htmlFor="Message" hidden>Message</label>
+                    <textarea type="text" id="Message" name="Message" placeholder="Message" required />
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
         </section>
     );
 }
