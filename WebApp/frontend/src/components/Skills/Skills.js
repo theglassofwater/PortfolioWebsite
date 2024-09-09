6f51ef2a216f21b586db2f2c59f08d88a79fd8a1
theglassofwater <joeace2002@gmail.com>
2024-09-09 14:49:56 +0100

    adding css to Skills

--- WebApp/frontend/src/components/Skills/Skills.js
+++ WebApp/frontend/src/components/Skills/Skills.js
@@ -1,17 +1,34 @@
 import React from "react";
-// import styles from "./Skills.module.css";
+import styles from "./Skills.module.css";
 import tick from "../../assets/tick.svg";
 import SkillsList from "./../Extra/SkillsList";
 
 const Skills = () => {
     return (
-        <section id="Skills">
+        <section id="Skills" className={styles.container}>
             <h1>
                 Skills
             </h1>
-            <div>   
+            <div className={styles.skillContainer}>   
+                <SkillsList icon={tick} name="Python"/>
+                <SkillsList icon={tick} name="SQL"/>
+                <SkillsList icon={tick} name="JavaScript"/>
+                <SkillsList icon={tick} name="HTML"/>
+                <SkillsList icon={tick} name="CSS"/>
+                <SkillsList icon={tick} name="Java"/>
+                <SkillsList icon={tick} name="MatLab"/>
+            </div>
+            <hr />
+            <div className={styles.skillContainer}>   
+                <SkillsList icon={tick} name="Data"/>
+                <SkillsList icon={tick} name="Data"/>
+                <SkillsList icon={tick} name="Data"/>
+                <SkillsList icon={tick} name="Data"/>
+                <SkillsList icon={tick} name="Data"/>
+                <SkillsList icon={tick} name="Data"/>
                 <SkillsList icon={tick} name="Data"/>
             </div>
+            <hr />
         </section>
     );
 }
