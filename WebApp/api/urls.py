b679094b55dd0bdd79db8cc0518745a1791f9449
theglassofwater <joeace2002@gmail.com>
2024-09-14 18:10:11 +0100

    added rest framework urls

--- WebApp/api/urls.py
+++ WebApp/api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from .views import *
 from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+# from views import UserViewSet, MessageViewSet
 
-urlpatterns = [
-    # path('', home),
-    path('', TemplateView.as_view(template_name='index.html')),
+router = DefaultRouter()
+router.register('users', UserViewSet, basename='user')
+router.register('message', MessageViewSet, basename='message')
+urlpatterns = router.urls
 
-]
+# urlpatterns = [
+#     path('homee/', home),
+#     path('', TemplateView.as_view(template_name='index.html')),
+
+# ]
