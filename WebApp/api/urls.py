4709aee3313a0d0b0a3a659a28d6b6be8354f47a
theglassofwater <joeace2002@gmail.com>
2024-09-19 19:37:07 +0100

    Refactor API URLs and add media URL configuration and generate song

--- WebApp/api/urls.py
+++ WebApp/api/urls.py
@@ -1,16 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
-# from views import UserViewSet, MessageViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
 router.register('messages', MessageViewSet, basename='messages')
-urlpatterns = router.urls
 
-# urlpatterns = [
-#     path('homee/', home),
-#     path('', TemplateView.as_view(template_name='index.html')),
+urlpatterns = [
+    # path('homee/', home),
+    path('', include(router.urls)),
+    path('generate_song/', generate_download_song, name='generate_song'),
 
-# ]
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
