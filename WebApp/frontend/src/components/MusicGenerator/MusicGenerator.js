87fe457e6ab44323dcddd65f41de3cdc108c66b5
theglassofwater <joeace2002@gmail.com>
2024-09-20 16:43:05 +0100

    works well now

--- WebApp/frontend/src/components/MusicGenerator/MusicGenerator.js
+++ WebApp/frontend/src/components/MusicGenerator/MusicGenerator.js
@@ -5,25 +5,39 @@ import ReactAudioPlayer from 'react-audio-player';
 import LoadingIcons from 'react-loading-icons'
 import styles from './MusicGenerator.module.css';
 
+let maxLength = 100;
+let duration;
+
 const MusicGenerator = () => {
     const [isGenerating, setIsGenerating] = useState(false);
-    const [song, setSong] = useState(false); 
     const [songInfo, setSongInfo] = useState(null);
 
-    const generate = async () => {
+    const generate = async (max_length) => {
         setIsGenerating(true);
-        setSong(false);
+        setSongInfo(null);
         try {
-            const response = await AxiosInstance.get('generate_song', {timeout: 60000}); // This generates but doesnt actually get the tokens
+            duration = Date.now();
+            const response = await AxiosInstance.get('generate_song', {timeout: 120000, params: {max_length: max_length}}); // This generates but doesnt actually get the tokens
+            duration = Date.now() - duration;
             setSongInfo(response.data);
-            setSong(true);
+            console.log(response.data);
+            console.log(duration);
 
         } catch (error) {
             console.error(error);
         };
 
         setIsGenerating(false);
-        console.log();
+    }
+
+    const handleLengthChange = (e) => {
+        maxLength = e.target.value;
+        if (maxLength > 2048) {
+            maxLength = 2048;
+        }
+        if (maxLength < 50) {
+            maxLength = 50;
+        }
     }
 
     return (
@@ -31,26 +45,39 @@ const MusicGenerator = () => {
             <h1>Music Generator</h1>
             
             <div className={styles.info}>
-                <p>May take up to a minute</p>
-                <button onClick={generate} disabled={isGenerating}>
+                <p>Generate a song using a Mistral model trained on MIDI files</p>
+                <label>Choose number of tokens (50-2048):</label>
+                <input
+                    id="maxLength"
+                    type="number"
+                    onChange={handleLengthChange}
+                />
+                <button onClick={() => generate(maxLength)} disabled={isGenerating}>
                     {isGenerating ? "Generating..." : "Generate Song"}
                 </button>
-                {isGenerating && <LoadingIcons.ThreeDots  className={styles.loading}/>}
+                {isGenerating && (
+                    <>
+                        <LoadingIcons.ThreeDots  className={styles.loading}/>
+                        <p>May take up to a minute(depending on number of tokens)</p>
+                    </>
+                )}
 
-                {song &&
+                {songInfo &&(
+                    <>
                     <ReactAudioPlayer
-                        src={songInfo.song_url}
+                        src={`${songInfo.song_url}?${global.Date.now()}`}
                         controls
                         className={styles.audio}
                     />
+                    <p>Generated in {String(duration)}ms</p>
+                    <img src={`${songInfo.img_url}?${global.Date.now()}`} alt="pianoroll" style={{ maxWidth: '1200px' }} />
+                    <p className={styles.tokens} >
+                        Tokens: Length of {songInfo.tokens.length} 
+                        <br />
+                         {songInfo.tokens.join(", ")}</p>
+                    </>
+                    )
                 }
-                {song && 
-                    <img src={songInfo.img_url} alt="pianoroll" style={{ maxWidth: '1200px' }} />
-                }
-
-                {song && <p>tokens: {songInfo.song}</p>}
-                
-
             </div>
         </section>
     );
