2f9ceb2a3f174a1fa378e40b2071e09361038eda
Youssef <joeace2002@gmail.com>
2025-02-04 12:28:38 +0000

    adding new urls for github.io to work

--- WebApp/frontend/src/App.js
+++ WebApp/frontend/src/App.js
@@ -6,12 +6,16 @@ import MusicGeneratorPage from './components/MusicGeneratorPage';
 import AimTrainerPage from './components/AimTrainerPage';
 
 function App() {
+  // the urls starting with PortfolioWebsite is for github.io, idk why it assumes that it needs to start with this
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/PortfolioWebsite" element={<HomePage />} /> 
         <Route path="/music-generator" element={<MusicGeneratorPage />} />
         <Route path="/aim-trainer" element={<AimTrainerPage />} />
+        <Route path="/PortfolioWebsite/music-generator" element={<MusicGeneratorPage />} />
+        <Route path="/PortfolioWebsite/aim-trainer" element={<AimTrainerPage />} />
       </Routes>
     </>
   );
